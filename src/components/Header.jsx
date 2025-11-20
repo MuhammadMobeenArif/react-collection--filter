69c144773ead4e047d972a91526b82aee40c75a6
MuhammadMobeenArif <mobeenarif13@gmail.com>
2025-11-20 13:59:23 +0500

    Remove unnecessary whitespace in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,10 +10,6 @@ function Header() {
           className="w-35 h-16 rounded-full"
         />
       </div>
-
- 
-
- 
   <div className="flex items-center gap-6">
     <a href="/" className="hover:text-yellow-400">Home</a>
     <a href="/products" className="hover:text-yellow-400">Products</a>
@@ -25,20 +21,15 @@ function Header() {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="size-6"
-          >
+            className="size-6" >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
               d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 
               2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 
-              5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-            />
+              5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
           </svg>
       </button>
- 
-
-
       </div>
     </div>
   );
